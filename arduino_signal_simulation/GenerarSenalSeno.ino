1731534a2b911eff0346399657aeae21ca0a63d0
Gilberto Galvis <gigalgi94@gmail.com>
2020-09-26 16:13:41 -0500

    Add files via upload

---
+++ arduino_signal_simulation/GenerarSenalSeno.ino
@@ -0,0 +1,28 @@
+
+double pi=3.14159265359;
+double f=50,sen;
+double t=0;
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  
+ while(Serial.available()){
+  for(int i=0;i<=512;i++){
+    delay(1);
+    sen=1023*sin(2*pi*f*t);
+    Serial.println(sen);
+  
+    t+=0.001 ;
+  }
+
+  f+=50;
+  t=0;
+  // delay in between reads for stability
+}
+
+}
